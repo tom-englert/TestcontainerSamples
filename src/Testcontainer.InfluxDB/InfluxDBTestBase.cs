f81847e6f3c0cf203341b2dd1d0a54b506e99672
tom-englert <mail@tom-englert.de>
2023-01-06 15:05:59 +0100

    Initial test: Testcontainer.InfluxDB

---
+++ src/Testcontainer.InfluxDB/InfluxDBTestBase.cs
@@ -0,0 +1,72 @@
+﻿using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Containers;
+using InfluxDB.Client;
+using InfluxDB.Client.Api.Domain;
+
+namespace Testcontainer.InfluxDB
+{
+    public abstract class InfluxDBTestBase : IAsyncLifetime
+    {
+        protected const string AdminToken = "my-super-secret-admin-token";
+
+        protected TestcontainersContainer TestContainer { get; private set; } = null!;
+        protected InfluxDBClient InfluxDbClient { get; private set; } = null!;
+        protected Bucket DefaultBucket { get; private set; } = null!;
+        protected ushort Port { get; private set; }
+
+        async Task IAsyncLifetime.InitializeAsync()
+        {
+            var environment = new Dictionary<string, string>
+            {
+                { "DOCKER_INFLUXDB_INIT_MODE", "setup" },
+                { "DOCKER_INFLUXDB_INIT_USERNAME" , "User" },
+                { "DOCKER_INFLUXDB_INIT_PASSWORD", "Password"},
+                { "DOCKER_INFLUXDB_INIT_ORG", "Org" },
+                { "DOCKER_INFLUXDB_INIT_BUCKET", "Bucket" },
+                { "DOCKER_INFLUXDB_INIT_ADMIN_TOKEN", AdminToken }
+            };
+
+            TestContainer = new TestcontainersBuilder<TestcontainersContainer>()
+                .WithName(Guid.NewGuid().ToString())
+                // .WithPortBinding(8086, true)
+                .WithPortBinding(49227, 8086)
+                .WithEnvironment(environment)
+                .WithImage("influxdb:latest")
+                .Build();
+
+            await TestContainer.StartAsync();
+            await TestContainer.ExecAsync(new[] { "influx" });
+
+            Port = TestContainer.GetMappedPublicPort(8086);
+
+            var address = $"http://localhost:{Port}";
+
+            var optionsBuilder = InfluxDBClientOptions.Builder.CreateNew()
+                .Url(address)
+                .Org("Org")
+                .AuthenticateToken(AdminToken);
+
+            InfluxDbClient = new InfluxDBClient(optionsBuilder.Build());
+
+            for (int i = 0; i < 100; i++)
+            {
+                var alive = await InfluxDbClient.PingAsync();
+                if (alive)
+                    break;
+                await Task.Delay(100);
+            }
+
+            var bucketsApi = InfluxDbClient.GetBucketsApi();
+
+            DefaultBucket = await bucketsApi.FindBucketByNameAsync("Bucket");
+
+            Assert.NotNull(DefaultBucket);
+        }
+
+        async Task IAsyncLifetime.DisposeAsync()
+        {
+            InfluxDbClient.Dispose();
+            await TestContainer.DisposeAsync();
+        }
+    }
+}
---
+++ src/Testcontainer.InfluxDB/UnitTest.cs
@@ -0,0 +1,70 @@
+using System.Text;
+using InfluxDB.Client.Api.Domain;
+using InfluxDB.Client.Core.Flux.Domain;
+using InfluxDB.Client.Writes;
+
+namespace Testcontainer.InfluxDB
+{
+    [UsesVerify]
+    public class UnitTest : InfluxDBTestBase
+    {
+        [Fact]
+        public async Task PointsCanBeWrittenAndQueried()
+        {
+            var influxDbClient = InfluxDbClient;
+            var bucket = DefaultBucket;
+            var now = DateTime.UtcNow;
+
+            var points = Enumerable.Range(1, 5)
+                .Select(i => PointData.Builder.Measurement("syslog")
+                    .Tag("tag1", $"{i}")
+                    .Tag("tag2", $"{i + 1}")
+                    .Field("field1", $"Data 1 {i}")
+                    .Field("field2", $"Data 2 {i}")
+                    .Timestamp(now + TimeSpan.FromMilliseconds(i), WritePrecision.Ms)
+                    .ToPointData())
+                .ToList();
+
+            await influxDbClient.GetWriteApiAsync().WritePointsAsync(points, bucket.Name, bucket.OrgID);
+
+            const string query = @"from(bucket: ""Bucket"")
+  |> range(start: -1h)
+  |> filter(fn: (r) => r[""_measurement""] == ""syslog"")
+  |> group(columns: [""_time""])
+";
+            var result = new List<QueryResult>();
+
+            await influxDbClient.GetQueryApi().QueryAsync(query, record =>
+            {
+                result.Add(new QueryResult(record));
+            });
+
+            await Verify(result);
+
+            //Console.WriteLine($"... Port: {port}");
+            //Console.ReadKey();
+        }
+
+        class QueryResult
+        {
+            public QueryResult(FluxRecord record)
+            {
+                Table = record.Table;
+                var values = record.Values;
+
+                Time = ((NodaTime.Instant)values["_time"]).ToDateTimeUtc();
+                Field = (string)values["_field"];
+                Value = values["_value"];
+                Tag1 = (string)values["tag1"];
+                Tag2 = (string)values["tag2"];
+            }
+
+            public DateTime Time { get; }
+            public int Table { get; }
+            public string Field { get; }
+            public object Value { get; }
+            public string Tag1 { get; }
+            public string Tag2 { get; }
+        }
+    }
+}
